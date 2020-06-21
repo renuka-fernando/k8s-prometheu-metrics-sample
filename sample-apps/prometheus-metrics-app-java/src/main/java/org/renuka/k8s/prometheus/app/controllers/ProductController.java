f5e39f7bd663d09a5e23224fc9f4a785c52ae8a8
Renuka Fernando <renuka@wso2.com>
2020-06-21 15:33:09 +0530

    Make sample application as products service

---
+++ sample-apps/prometheus-metrics-app-java/src/main/java/org/renuka/k8s/prometheus/app/controllers/ProductController.java
@@ -0,0 +1,94 @@
+package org.renuka.k8s.prometheus.app.controllers;
+
+import io.prometheus.client.Counter;
+import org.renuka.k8s.prometheus.app.dto.Product;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductController {
+    // metrics: total request counter
+    private static final Counter reqCount = Counter.build().name("hello_http_requests_total")
+            .labelNames("method", "priority").help("hello: total http requests").register();
+
+    // products
+    private static Map<Integer, Product> products = new HashMap<Integer, Product>();
+    private static Integer lastProductId = 105;
+
+    // load products
+    static {
+        products.put(101, new Product(101, "Apples", "Food", "$1.49"));
+        products.put(102, new Product(102, "Macaroni & Cheese", "Food", "$7.69"));
+        products.put(103, new Product(102, "ABC Smart TV", "Electronics", "$399.99"));
+        products.put(104, new Product(104, "Motor Oil", "Automobile", "$22.88"));
+        products.put(105, new Product(105, "Floral Sleeveless Blouse", "Clothing", "$21.50"));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<Collection<Product>> getProducts() {
+        reqCount.labels("GET", "HIGH").inc();
+        return ResponseEntity.ok(products.values());
+    }
+
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<Product> getProducts(@PathVariable("productId") Integer productId) {
+        reqCount.labels("GET", "HIGH").inc();
+
+        Product product = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) throws URISyntaxException {
+        reqCount.labels("POST", "HIGH").inc();
+
+        Integer productId = ++lastProductId;
+        product.setProductId(productId);
+        products.put(productId, product);
+        return ResponseEntity.created(new URI("/products/" + productId)).body(product);
+    }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("productId") Integer productId,
+                                                 @RequestBody Product product) throws URISyntaxException {
+        reqCount.labels("PUT", "LOW").inc();
+
+        Product productOld = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        product.setProductId(productId);
+        products.replace(productId, product);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("productId") Integer productId) {
+        reqCount.labels("GET", "HIGH").inc();
+
+        Product product = products.get(productId);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        products.remove(productId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ sample-apps/prometheus-metrics-app-java/src/main/java/org/renuka/k8s/prometheus/app/dto/Product.java
@@ -0,0 +1,47 @@
+package org.renuka.k8s.prometheus.app.dto;
+
+public class Product {
+    int productId;
+    String name;
+    String category;
+    String price;
+
+    public Product(int productId, String name, String category, String price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
